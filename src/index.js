7c33c56caf008b89301b51a647c94dbf669b124f
baadillahnabil <nabilzero27@gmail.com>
2019-10-04 13:28:59 +0700

    add theme. add router root. add snackbar setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@material-ui/styles'
 import { BrowserRouter } from 'react-router-dom'
+import { SnackbarProvider } from 'notistack'
+import theme from './theme'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <SnackbarProvider
+        maxSnack={1}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        autoHideDuration={2000}
+      >
+        <App />
+      </SnackbarProvider>
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root')
 )
 
